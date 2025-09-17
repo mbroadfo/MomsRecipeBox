595e43df583fb1a3c80ad12d7c86324f7b21d40e
Mike Broadfoot <mbroadfo@yahoo.com>
2025-09-17 15:11:30 -0600

    Removed Secrets Management Capability

--- infra/mongodb_atlas_variables.tf
+++ infra/mongodb_atlas_variables.tf
@@ -10,7 +10,3 @@
 # - mongodb_atlas_password: Password for the MongoDB Atlas database user
 # - development_cidr_block: CIDR block for development access (your IP address with /32)
 # - lambda_cidr_block: CIDR block for AWS Lambda access (if using Lambda)
-
-# To manage these values, use the scripts:
-# - scripts/Get-MongoAtlasSecrets.ps1: Retrieves secrets from AWS Secrets Manager
-# - scripts/Set-MongoAtlasSecret.ps1: Creates or updates secrets in AWS Secrets Manager
